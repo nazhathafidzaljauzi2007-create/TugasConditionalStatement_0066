f86ba278f3a6e7b11cf07eff7c0b8ac526ce9ffa
nazhat <nazhathafidzaljauzi2007@gmail.com>
2026-03-10 21:33:41 +0700

    Update hitungBMI.cpp

--- hitungBMI.cpp
+++ hitungBMI.cpp
@@ -23,7 +23,7 @@ string kondisiBMI(float bmi)
     if(bmi < 18.5)
        return "Berat Badan Kurang";
     else if (bmi < 25)
-       return "Berat Badan Normal";
+       return "Berat Badan Normal"; 
     else if (bmi < 30)
        return "Berat Badan Kelebihan";
     else
