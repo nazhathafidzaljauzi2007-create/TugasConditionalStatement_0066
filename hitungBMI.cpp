68e2abbd45b37b3745d4cb8d829ce0aebe8e298c
nazhat <nazhathafidzaljauzi2007@gmail.com>
2026-03-10 21:33:31 +0700

    Update hitungBMI.cpp

--- hitungBMI.cpp
+++ hitungBMI.cpp
@@ -7,7 +7,7 @@ void inputData(float &berat, float &tinggi)
     cout << "Masukkan berat badan (kg): ";
     cin >> berat;
 
-    cout << "Masukkan tinggi badan (meter): ";
+    cout << "Masukkan tinggi badan (meter): "; 
     cin >> tinggi;
 }
 
